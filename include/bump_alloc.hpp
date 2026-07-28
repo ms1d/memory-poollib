cbf2ecf4c8b08c3e7f7ed82fb80a9b97422caf02
Mahad Siddiqui <mahadsiddiqui07@outlook.com>
2026-07-28 18:09:16 +0100

    fixed so it compiles

--- include/bump_alloc.hpp
+++ include/bump_alloc.hpp
@@ -3,7 +3,6 @@
 
 #include <cassert>
 #include <cstdint>
-#include <cstdlib>
 
 
 
@@ -16,12 +15,12 @@ public:
 
 
 	bump_pool() {
-		data = malloc(sizeof(obj) * pool_capacity);
+		data = new obj[pool_capacity];
 		head = data;
 	}
 
 	~bump_pool() {
-        free(data);
+        delete[] data;
     }
 
 
@@ -38,7 +37,7 @@ public:
 
 private:
 	
-	const obj *data;
+	obj *data;
 	obj *head;
 
 
