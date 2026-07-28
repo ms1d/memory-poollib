7ea9039092c4cf6d38801ec9b1d386262816a312
Mahad Siddiqui <mahadsiddiqui07@outlook.com>
2026-07-28 22:08:26 +0100

    implemented thread unsafe arena

--- include/arena_pool.hpp
+++ include/arena_pool.hpp
@@ -1,10 +1,63 @@
 #pragma once
 
+
 #include "bump_pool.hpp"
 #include <vector>
 
+
+
+template<typename obj, uint32_t chunk_capacity = 1024, mp_type pool_type = mp_type::thread_unsafe>
+class arena;
+
+
+template<typename obj, uint32_t chunk_capacity>
+class arena<obj, chunk_capacity, mp_type::thread_unsafe> {
+	
+
+public:
+
+	arena() {
+		chunks.emplace_back();
+	}
+
+	~arena() {
+		free();
+	}
+
+
+	obj *alloc(uint32_t count = 1) {
+		if (count > chunk_capacity) return nullptr;
+
+		auto &c = chunks[current_chunk];
+
+		auto res = c.alloc(count);
+
+		if (res == nullptr) {
+			chunks.emplace_back();
+			current_chunk++;
+			return chunks[current_chunk].alloc(count);
+		} else { return res; }
+    }
+
+	// Free n chunks (defaults to 1). If n is larger than current number of chunks, free all.
+	void free(uint32_t n = 1) {
+		for (int i = chunks.size() - 1; i >= 0 && n >= 0; i--, n--) {
+            chunks[i].free();
+        }
+	}
+
+
+private:
+
+	std::vector<bump_pool<obj, chunk_capacity, mp_type::thread_unsafe>> chunks;
+	uint32_t current_chunk = 0;
+
+
+};
+
+
 template<typename obj>
-class arena {
+class arena<obj, mp_type::thread_safe> {
 	
 
 public:
